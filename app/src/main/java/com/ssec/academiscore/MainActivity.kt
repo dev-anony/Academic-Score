d712171f372d9c37cdef54b00bd8bcbad3ebef36
VIJAY <vs0562944@gmail.com>
2020-02-05 14:03:03 +0530

    build 1

---
+++ app/src/main/java/com/ssec/academiscore/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ssec.academiscore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
