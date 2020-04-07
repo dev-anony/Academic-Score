6d8c139031d3b938b52edaa1ebe05b6b534d52d2
VIJAY <vs0562944@gmail.com>
2020-04-08 05:25:29 +0530

    frag_Result.xml completed

--- app/src/main/java/com/ssec/academiscore/FragmentResult.kt
+++ app/src/main/java/com/ssec/academiscore/FragmentResult.kt
@@ -15,19 +15,59 @@ class FragmentResult : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val t=inflater.inflate(R.layout.fragment_result, container, false)
-        val languages = resources.getStringArray(R.array.Languages)
+
+        //setting up course spiner
+
+        val courses = resources.getStringArray(R.array.courses)
         val spinner = t.findViewById<Spinner>(R.id.spinner)
-        spinner?.adapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, languages)
+        spinner?.adapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, courses)
         spinner?.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 println("erreur")
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val type = parent?.getItemAtPosition(position).toString()
-                Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
-                println(type)
+                //val type = parent?.getItemAtPosition(position).toString()
+                //Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
+                //println(type)
+            }
+
+        }
+
+        //setting up semester spinner
+
+        val semester = resources.getStringArray(R.array.semester)
+        val spinner2 = t.findViewById<Spinner>(R.id.spinner2)
+        spinner2?.adapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, semester)
+        spinner2?.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //val type = parent?.getItemAtPosition(position).toString()
+                //Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
+                //println(type)
+            }
+
+        }
+
+        //setting up test spinner
+
+        val test = resources.getStringArray(R.array.test)
+        val spinner3 = t.findViewById<Spinner>(R.id.spinner3)
+        spinner3?.adapter = ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, test)
+        spinner3?.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //val type = parent?.getItemAtPosition(position).toString()
+                //Toast.makeText(activity,type, Toast.LENGTH_LONG).show()
+                //println(type)
             }
 
         }
